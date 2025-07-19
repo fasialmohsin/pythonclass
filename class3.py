feeb4b4d9d2d162127f71850b00b205aaad0bde1
faisal <ranafaisalg85@gmail.com>
2025-07-19 22:38:27 +0500

    class3.py

---
+++ class3.py
@@ -0,0 +1,12 @@
+# -------------- Input and Output----------------
+
+#  `input()` function
+
+# name = input("enter your name: ")
+# print(f"You are welcome Mr.{name}")
+
+# Type conversion (`int()`, `float()`, etc.)
+
+x = "10"
+y = bool(x)
+print(type(y))
